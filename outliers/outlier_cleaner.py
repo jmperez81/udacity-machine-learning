3aebfa49f01ce9b3d56262e3cd135603d0b7bd88
Juan Manuel Pérez <juanmanuel.perez@adidas,com>
2020-05-01 15:37:34 +0200

    Finished lesson 7 : Outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-
+from numpy import subtract, square, argmin, take, delete
+import sys
 
 def outlierCleaner(predictions, ages, net_worths):
     """
@@ -12,9 +13,23 @@ def outlierCleaner(predictions, ages, net_worths):
     """
     
     cleaned_data = []
-
-    ### your code goes here
-
     
-    return cleaned_data
+    # Calculation of square error
+    sqe = square(subtract(predictions, net_worths))
 
+    # Calculating target number of elements (90%)
+    m = len(ages)
+    desired_m = (90 * m) / 100
+    print("Reducing training set from ", m, " to ", desired_m)
+
+    # Iteration to keep 90 samples with smaller errors
+    i = 0
+    while i < desired_m:
+        min_sqe_index = argmin(sqe)        
+        min_sqe_value = take(sqe, min_sqe_index)
+        print(min_sqe_value)
+        sqe[min_sqe_index] = sys.maxint
+        cleaned_data.append((ages[min_sqe_index], net_worths[min_sqe_index], min_sqe_value))
+        i = i + 1
+
+    return cleaned_data
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -12,8 +12,6 @@ from outlier_cleaner import outlierCleaner
 ages = pickle.load( open("practice_outliers_ages.pkl", "r") )
 net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "r") )
 
-
-
 ### ages and net_worths need to be reshaped into 2D numpy arrays
 ### second argument of reshape command is a tuple of integers: (n_rows, n_columns)
 ### by convention, n_rows is the number of data points
@@ -25,16 +23,12 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
-
-
-
-
-
-
-
-
-
-
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
+print("Slope is : ", reg.coef_)
+print("Intercept is :", reg.intercept_)
+print("r-square for regression : ", reg.score(ages_test, net_worths_test))
 
 try:
     plt.plot(ages, reg.predict(ages), color="blue")
@@ -53,12 +47,6 @@ except NameError:
     print "your regression object doesn't exist, or isn't name reg"
     print "can't make predictions to use in identifying outliers"
 
-
-
-
-
-
-
 ### only run this code if cleaned_data is returning data
 if len(cleaned_data) > 0:
     ages, net_worths, errors = zip(*cleaned_data)
@@ -68,6 +56,10 @@ if len(cleaned_data) > 0:
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
+        print("Slope is : ", reg.coef_)
+        print("Intercept is :", reg.intercept_)
+        print("r-square for regression : ", reg.score(ages_test, net_worths_test))
+
         plt.plot(ages, reg.predict(ages), color="blue")
     except NameError:
         print "you don't seem to have regression imported/created,"
